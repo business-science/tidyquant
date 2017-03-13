fe3f9b29bed4a5a445f114e7633225af124f3552
mdancho <mdancho@gmail.com>
2017-03-13 13:45:33 -0400

    fix note: bind select_

--- R/tq_portfolio.R
+++ R/tq_portfolio.R
@@ -270,7 +270,7 @@ tq_portfolio_base_ <- function(data, assets_col, returns_col, weights, col_renam
 
         # Spread for xts form and apply Return.portfolio()
         data %>%
-            select_(date_col_name, assets_col_name, returns_col_name) %>%
+            dplyr::select_(date_col_name, assets_col_name, returns_col_name) %>%
             tidyr::spread_(key_col = assets_col_name, value_col = returns_col_name) %>%
             as_xts_(date_col = date_col_name) %>%
             PerformanceAnalytics::Return.portfolio(weights = weights, verbose = FALSE, ...)
