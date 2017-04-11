af40e1a1a9694c2a838959bbda129678ffb7ec1f
DavisVaughan <mvaugh15@uncc.edu>
2017-04-10 22:25:20 -0400

    Fix tests for Oanda problems. Works with dev quantmod branch now.

--- tests/testthat/test_tq_get_exchange_rates.R
+++ tests/testthat/test_tq_get_exchange_rates.R
@@ -5,12 +5,12 @@ get <- "exchange.rates"
 context(paste0("Testing tq_get(get = '", get, "')"))
 
 test1 <- tq_get("EUR/USD", get = get,
-                from = "2016-01-01", to = "2016-06-01",
+                from = "2017-01-01", to = "2017-04-01",
                 adjust = TRUE, type = "splits")
 
 
 test2 <- tq_get("EUR/USD", get = get,
-                from = "2016-01-01", to = "2016-06-01",
+                from = "2017-01-01", to = "2017-04-01",
                 adjust = FALSE, type = "price")
 
 #### Tests
@@ -22,7 +22,7 @@ test_that("Test returns tibble with correct rows and columns.", {
     # Identical
     expect_identical(test1, test2)
     # Rows
-    expect_equal(nrow(test1), 153)
+    expect_equal(nrow(test1), 91)
     # Columns
     expect_equal(ncol(test1), 2)
 })
--- tests/testthat/test_tq_get_metal_prices.R
+++ tests/testthat/test_tq_get_metal_prices.R
@@ -1,16 +1,17 @@
 library(tidyquant)
 
 #### Setup
+
 get <- "metal.prices"
 context(paste0("Testing tq_get(get = '", get, "')"))
 
 test1 <- tq_get("gold", get = get,
-                from = "2016-01-01", to = "2016-06-01",
+                from = "2017-01-01", to = "2017-04-01",
                 adjust = TRUE, type = "splits")
 
 
 test2 <- tq_get("gold", get = get,
-                from = "2016-01-01", to = "2016-06-01",
+                from = "2017-01-01", to = "2017-04-01",
                 adjust = FALSE, type = "price")
 
 #### Tests
@@ -22,7 +23,7 @@ test_that("Test returns tibble with correct rows and columns.", {
     # Identical
     expect_identical(test1, test2)
     # Rows
-    expect_equal(nrow(test1), 153)
+    expect_equal(nrow(test1), 91)
     # Columns
     expect_equal(ncol(test1), 2)
 })
