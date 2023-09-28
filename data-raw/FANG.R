732f894e3541cc6bfb3944b14372078a4040b4ac
olivroy <olivierroy71@hotmail.com>
2023-09-28 19:42:39 -0400

    Put META in FANG.

--- data-raw/FANG.R
+++ data-raw/FANG.R
@@ -1,3 +1,5 @@
 ## code to prepare `FANG` dataset goes here
 FANG <- readr::read_csv("data-raw/FANG.csv")
+FANG <- FANG %>%
+    mutate(symbol = ifelse(symbol == "FB", "META", symbol))
 usethis::use_data(FANG, overwrite = TRUE)
